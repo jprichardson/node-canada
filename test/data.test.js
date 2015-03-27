74ac72a925ff25331b0221125794e312dd8742e3
JP Richardson <jprichardson@gmail.com>
2015-03-27 01:56:14 -0500

    test/data: verify JSON valid

---
+++ test/data.test.js
@@ -0,0 +1,15 @@
+/* global describe, it */
+
+describe('data', function () {
+  describe('cities', function () {
+    it('should parse', function () {
+      require('../data/cities')
+    })
+  })
+
+  describe('provinces', function () {
+    it('should parse', function () {
+      require('../data/provinces')
+    })
+  })
+})
